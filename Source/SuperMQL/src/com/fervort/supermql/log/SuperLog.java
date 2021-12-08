b0af811ef43c49e7f9d184843372c8983a93692b
DC\Shams <mshamsj@gmail.com>
2021-12-08 10:01:24 +0330

    Init

--- Source/SuperMQL/src/com/fervort/supermql/log/SuperLog.java
+++ Source/SuperMQL/src/com/fervort/supermql/log/SuperLog.java
@@ -4,22 +4,19 @@ import com.fervort.supermql.xml.*;
 
 public class SuperLog {
 
-	private static boolean isDebugEnabled = false;
-	
-	public static void init()
-	{
-		if(ConfigReader.readConfigKey("EnableDebugging").equalsIgnoreCase("yes"))
-		{
-			isDebugEnabled = true;
-			System.out.println("=====Debugging Enabled=====");
-		}
-	}
-	
-	public static void debug(String str)
-	{
-		if(isDebugEnabled)
-		{
-			System.out.println(str);
-		}
-	}
+    private static boolean isDebugEnabled = false;
+    private static final String CONFIG_KEY = "EnableDebugging";
+
+    public static void init() {
+        if (ConfigReader.readConfigKey(CONFIG_KEY).equalsIgnoreCase("yes")) {
+            isDebugEnabled = true;
+            System.out.println("=====Debugging Enabled=====");
+        }
+    }
+
+    public static void debug(String str) {
+        if (isDebugEnabled) {
+            System.out.println(str);
+        }
+    }
 }
